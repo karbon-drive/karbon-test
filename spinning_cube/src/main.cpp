8a5281b4f7b606bcc1f39dc5c491588dd9e193f3
Phil Cooper-King <phil@cooperking.net>
2018-11-23 08:17:47 +0000

    more dx stuff

--- spinning_cube/src/main.cpp
+++ spinning_cube/src/main.cpp
@@ -34,6 +34,11 @@ struct spinning_cube_ctx {
         #if KD_WINDOWS
         ID3D12Device *d3d_device;
         ID3D12CommandQueue *cmd_queue;
+        ID3D12CommandAllocator* cmd_alloc;
+        ID3D12GraphicsCommandList* cmd_list;
+
+        ID3D12Resource *backbuffer[2];
+        UINT backbuffer_index;
         #endif
 };
 
@@ -97,6 +102,16 @@ kd_setup() {
 
         int card_mem = (int)(adapter_desc.DedicatedVideoMemory / 1024 / 1024);
 
+        DXGI_MODE_DESC mode_desc{};
+        for (auto l : list) {
+                if (l.Height == 480) {
+                        if (l.Width == 720) {
+                                mode_desc = l;
+                                break;
+                        }
+                }
+        }
+
         /* swap chain */
         DXGI_SWAP_CHAIN_DESC swap_desc{};
         swap_desc.BufferCount = 2;
@@ -111,20 +126,98 @@ kd_setup() {
         swap_desc.OutputWindow = hwnd;
         swap_desc.Windowed = true;
 
-        swap_desc.BufferDesc.RefreshRate.Numerator = 0; //list[0].RefreshRate.Numerator;
-        swap_desc.BufferDesc.RefreshRate.Denominator = 1; //list[0].RefreshRate.Denominator;
-
+        swap_desc.BufferDesc.RefreshRate.Numerator = mode_desc.RefreshRate.Numerator;
+        swap_desc.BufferDesc.RefreshRate.Denominator = mode_desc.RefreshRate.Denominator;
         swap_desc.SampleDesc.Count = 1;
-        swap_desc.SampleDesc.Quality = 1;
-
+        swap_desc.SampleDesc.Quality = 0;
         swap_desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
         swap_desc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
-
         swap_desc.Flags = 0;
 
-        IDXGISwapChain *swap_chain = nullptr;
-        res = factory->CreateSwapChain(cmd_queue, &swap_desc, &swap_chain);
+        IDXGISwapChain *swap_chain_ = nullptr;
+        res = factory->CreateSwapChain(cmd_queue, &swap_desc, &swap_chain_);
         assert(!FAILED(res));
+
+        IDXGISwapChain3 *swap_chain = nullptr;
+        void ** pp_swap = (void**)&swap_chain;
+        ref_id = __uuidof(IDXGISwapChain3);
+
+        res = swap_chain_->QueryInterface(ref_id, pp_swap);
+
+        factory->Release();
+        factory = 0;
+
+        D3D12_DESCRIPTOR_HEAP_DESC rt_heap_desc{};
+        rt_heap_desc.NumDescriptors = 2;
+        rt_heap_desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+        rt_heap_desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+
+        ID3D12DescriptorHeap *desc_heap = nullptr;
+
+        ref_id = __uuidof(ID3D12DescriptorHeap);
+        void **pp_rtvh = (void**)&desc_heap;
+
+        res = dev->CreateDescriptorHeap(&rt_heap_desc, ref_id, pp_rtvh);
+        assert(!FAILED(res));
+
+        auto heap_start = desc_heap->GetCPUDescriptorHandleForHeapStart();
+        auto rtv_desc_size = dev->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+        ref_id = __uuidof(ID3D12Resource);
+        ID3D12Resource *back_buffer_rt[2]{};
+        void **pp_backbuffer_rt = (void**)&back_buffer_rt[0];
+        res = swap_chain->GetBuffer(0, ref_id, pp_backbuffer_rt);
+        assert(!FAILED(res));
+
+        dev->CreateRenderTargetView(back_buffer_rt[0], NULL, heap_start);
+
+        heap_start.ptr += rtv_desc_size;
+
+        pp_backbuffer_rt = (void**)&back_buffer_rt[1];
+        res = swap_chain->GetBuffer(1, ref_id, pp_backbuffer_rt);
+        assert(!FAILED(res));
+
+        auto index = swap_chain->GetCurrentBackBufferIndex();
+
+        spin_ctx.backbuffer[0] = back_buffer_rt[0];
+        spin_ctx.backbuffer[1] = back_buffer_rt[1];
+        spin_ctx.backbuffer_index = index;
+
+        ID3D12CommandAllocator* cmd_alloc = nullptr;
+        auto alloc_type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+        ref_id = __uuidof(ID3D12CommandAllocator);
+        void **pp_cmd_alloc = (void**)&cmd_alloc;
+        res = dev->CreateCommandAllocator(alloc_type, ref_id, pp_cmd_alloc);
+        assert(!FAILED(res));
+
+        spin_ctx.cmd_alloc = cmd_alloc;
+
+        auto cmd_type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+        ref_id = __uuidof(ID3D12GraphicsCommandList);
+        ID3D12GraphicsCommandList* cmd_list = nullptr;
+        void **pp_cmd_list = (void**)&cmd_list;
+        res = dev->CreateCommandList(0, cmd_type, cmd_alloc, NULL, ref_id, pp_cmd_list);
+        assert(!FAILED(res));
+        cmd_list->Close();
+
+        spin_ctx.cmd_list = cmd_list;
+
+        ID3D12Fence* fence = nullptr;;
+        auto fflag = D3D12_FENCE_FLAG_NONE;
+        ref_id = __uuidof(ID3D12Fence);
+        void **pp_fence = (void**)&fence;
+        res = dev->CreateFence(0, fflag, ref_id, pp_fence);
+        assert(!FAILED(res));
+
+        HANDLE fence_evt = 0;
+
+        fence_evt = CreateEventEx(NULL, FALSE, FALSE, EVENT_ALL_ACCESS);
+        assert(fence_evt);
+
+        unsigned long long fence_value;
+
+
+        /* http://www.rastertek.com/dx12tut03.html */
 
         /*
         IDXGIAdapter* adapter;
@@ -138,6 +231,21 @@ kd_setup() {
         IDXGISwapChain* swapChain;
         D3D12_DESCRIPTOR_HEAP_DESC renderTargetViewHeapDesc;
         D3D12_CPU_DESCRIPTOR_HANDLE renderTargetViewHandle;
+
+        bool m_vsync_enabled;
+        ID3D12Device* m_device;
+        ID3D12CommandQueue* m_commandQueue;
+        char m_videoCardDescription[128];
+        IDXGISwapChain3* m_swapChain;
+        ID3D12DescriptorHeap* m_renderTargetViewHeap;
+        ID3D12Resource* m_backBufferRenderTarget[2];
+        unsigned int m_bufferIndex;
+        ID3D12CommandAllocator* m_commandAllocator;
+        ID3D12GraphicsCommandList* m_commandList;
+        ID3D12PipelineState* m_pipelineState;
+        ID3D12Fence* m_fence;
+        HANDLE m_fenceEvent;
+        unsigned long long m_fenceValue;
         */
 
         /* save d3d items */
@@ -169,8 +277,32 @@ kd_think() {
 
 KD_API KD_EXPORT void
 kd_late_think() {
-        int i;
-        (void)i;
+        HRESULT result;
+        D3D12_RESOURCE_BARRIER barrier;
+        D3D12_CPU_DESCRIPTOR_HANDLE rtv_handle;
+        unsigned int rtv_desc_size;
+        float color[4];
+        ID3D12CommandList* pp_cmd_list[1];
+        unsigned long long fence_wait_for;
+        ID3D12PipelineState* pipeline = 0;
+
+        result = spin_ctx.cmd_alloc->Reset();
+        assert(!FAILED(result));
+
+        result = spin_ctx.cmd_list->Reset(spin_ctx.cmd_alloc, pipeline);
+        assert(!FAILED(result));
+
+        barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+        UINT index = spin_ctx.backbuffer_index;
+        barrier.Transition.pResource = spin_ctx.backbuffer[index];
+
+        barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
+        barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+        barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+        barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+        spin_ctx.cmd_list->ResourceBarrier(1, &barrier);
+
+        /* http://www.rastertek.com/dx12tut03.html */
 }
 
 
